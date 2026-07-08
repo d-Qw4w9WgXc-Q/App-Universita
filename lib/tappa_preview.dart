4f292e64d5f7948ecb68e60f611d53088ab4d564
vincenzo <42910242+d-Qw4w9WgXc-Q@users.noreply.github.com>
2026-07-08 18:39:41 +0200

    Menu tappe

---
+++ lib/tappa_preview.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TappaPreview extends StatefulWidget {
+        late final UniqueKey id;
+
+        TappaPreview({super.key}) {
+                id = UniqueKey();
+        }
+
+
+        @override
+        State<TappaPreview> createState() => _TappaPreviewState();
+}
+
+class _TappaPreviewState extends State<TappaPreview> {
+        @override
+        Widget build(BuildContext context) {
+                return TextField(decoration: InputDecoration(hintText: 'Tappa'));
+        }
+}
--- lib/tappe.dart
+++ lib/tappe.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'tappa_preview.dart';
 
 class Tappe extends StatefulWidget {
         const Tappe({super.key});
@@ -7,13 +8,50 @@ class Tappe extends StatefulWidget {
         State<Tappe> createState() => _TappeState();
 }
 
+
 class _TappeState extends State<Tappe> {
+        final List<TappaPreview> _tappe = [];
+
         @override
         Widget build(BuildContext context) {
                 return Scaffold(
                         appBar: AppBar(
                                 backgroundColor: Theme.of(context).colorScheme.inversePrimary,
                                 title: Text('Tappe'),
+                        ),
+                        body: ReorderableListView.builder(
+                                buildDefaultDragHandles: false,
+                                itemCount: _tappe.length,
+                                itemBuilder: (context, index) => ListTile(
+                                        title: _tappe[index],
+                                        key: ObjectKey(_tappe[index].id),
+
+                                        trailing: ReorderableDragStartListener(
+                                                index: index,
+                                                child: Listener(
+                                                        onPointerDown: (_) {
+                                                                FocusManager.instance.primaryFocus?.unfocus();
+                                                        },
+                                                        child:Icon(Icons.drag_handle)
+                                                )
+                                        ),
+                                ),
+
+                                onReorderItem: (int oldIndex, int newIndex) {
+                                        setState(() {
+
+                                                final item = _tappe.removeAt(oldIndex);
+                                                _tappe.insert(newIndex, item);
+                                        });
+                                }
+                        ),
+                        floatingActionButton: FloatingActionButton(
+                                onPressed: () {
+                                        setState(() {
+                                                _tappe.add(TappaPreview());
+                                        });
+                                },
+                                child: Icon(Icons.add)
                         ),
                 );
         }
